5ae524b153da563ed8b01c5a21a8772256b03f8d
0000yuyu <yujin09219@naver.com>
2024-08-28 15:08:59 +0900

    [송유진] 각 패널에 연결되어 있는 텍스트들에 접근하기(GameManager.cs 수정)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -250,16 +250,33 @@ public class GameManager : MonoBehaviour
     public long SubWeapon4(long amount){ Instance.weapon1-=amount;SetUIText(); return Instance.weapon4; }
 
 
-    //버튼 클릭 재화 함수
+    //현재 재화의 양, 필요한 돈 바꾸기 위한 변수
+
+    //버튼에는 골드가 저장 , amount 에는 현재 재화의 양을 저장
+
+    TextMeshProUGUI CostText;
+    TextMeshProUGUI AmountText;
+
+
+    //버튼 클릭 재화 함수 -> 텍스트, 버튼 텍스트 모두 변경
     public void OnButtonClickRock()
     {
+
         if (GetGold() <= UpgradeRockCost)
             Debug.Log("골드 부족");
         else 
         {
-            SubGold(UpgradeMissionaryCost);
-            AddRock(UpgradeMissionaryCost);
+            SubGold(UpgradeRockCost);
+            AddRock(UpgradeRockCost);
         }
+
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+        CostText = ResourcePanel.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = ResourcePanel.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+
+        //이제 텍스트 변경
+        CostText.text = UpgradeRockCost+"";
+        AmountText.text = GetRock()+"";
     }
     public void OnButtonClickTree()
     {
@@ -270,6 +287,13 @@ public class GameManager : MonoBehaviour
             SubGold(UpgradeTreeCost);
             AddTree(UpgradeTreeCost);
         }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+        CostText = ResourcePanel.transform.GetChild(0).GetChild(0).GetChild(1).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = ResourcePanel.transform.GetChild(0).GetChild(0).GetChild(1).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeTreeCost+"";
+        AmountText.text = GetTree()+"";
     }
     public void OnButtonClickBread()
     {
@@ -280,6 +304,14 @@ public class GameManager : MonoBehaviour
             SubGold(UpgradeBreadCost);
             AddBread(UpgradeBreadCost);
         }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = ResourcePanel.transform.GetChild(0).GetChild(0).GetChild(2).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = ResourcePanel.transform.GetChild(0).GetChild(0).GetChild(2).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeBreadCost+"";
+        AmountText.text = GetBread()+"";
     }
     public void OnButtonClickMissionary()
     {
@@ -290,6 +322,15 @@ public class GameManager : MonoBehaviour
             SubGold(UpgradeMissionaryCost);
             AddMissionary(UpgradeMissionaryCost);
         }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = MissionaryPanel.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = MissionaryPanel.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeMissionaryCost+"";
+        AmountText.text = GetMissionary()+"";
+    
     }
     public void OnButtonClickFanatic()
     {
@@ -300,6 +341,14 @@ public class GameManager : MonoBehaviour
             SubGold(UpgradeFanaticCost);
             AddFanatic(UpgradeFanaticCost);
         }
+                //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = MissionaryPanel.transform.GetChild(0).GetChild(0).GetChild(1).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = MissionaryPanel.transform.GetChild(0).GetChild(0).GetChild(1).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeFanaticCost+"";
+        AmountText.text = GetFanatic()+"";
     }
     public void OnButtonClickCardinal()
     {
@@ -310,6 +359,18 @@ public class GameManager : MonoBehaviour
             SubGold(UpgradeCardinalCost);
             AddCardinal(UpgradeCardinalCost);
         }
+
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = MissionaryPanel.transform.GetChild(0).GetChild(0).GetChild(2).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = MissionaryPanel.transform.GetChild(0).GetChild(0).GetChild(2).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeCardinalCost+"";
+        AmountText.text = GetCardinal()+"";
+        
+        
+        
     }
     public void OnButtonClickAdult()
     {
@@ -320,6 +381,14 @@ public class GameManager : MonoBehaviour
             SubGold(UpgradeAdultCost);
             AddAdult(UpgradeAdultCost);
         }
+                //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = MissionaryPanel.transform.GetChild(0).GetChild(0).GetChild(3).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = MissionaryPanel.transform.GetChild(0).GetChild(0).GetChild(3).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeMissionaryCost+"";
+        AmountText.text = GetAdult()+"";
     }
     public void OnButtonClickDragoon()
     {
@@ -330,6 +399,15 @@ public class GameManager : MonoBehaviour
             SubGold(UpgradeDragoonCost);
             AddDragoon(UpgradeDragoonCost);
         }
+
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = MissionaryPanel.transform.GetChild(0).GetChild(0).GetChild(4).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = MissionaryPanel.transform.GetChild(0).GetChild(0).GetChild(4).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeDragoonCost+"";
+        AmountText.text = GetDragoon()+"";
     }
     public void OnButtonClickHut()
     {
@@ -340,6 +418,14 @@ public class GameManager : MonoBehaviour
             SubGold(UpgradeHutCost);
             AddHut(UpgradeHutCost);
         }
+                //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeHutCost+"";
+        AmountText.text = GetHut()+"";
     }
     public void OnButtonClickChurch2()
     {
@@ -350,82 +436,212 @@ public class GameManager : MonoBehaviour
             SubGold(UpgradeChurch2Cost);
             AddChurch2(UpgradeChurch2Cost);
         }
+                        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(1).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(1).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeChurch2Cost+"";
+        AmountText.text = GetChurch2()+"";
     }
     public void OnButtonClickChurch3()
     {
         if (GetGold() <= UpgradeChurch3Cost)
             Debug.Log("골드 부족");
-        else
+        else{
+
             SubGold(UpgradeChurch3Cost);
+            AddChurch3(UpgradeChurch3Cost);
+        }
+
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(2).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(2).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeChurch3Cost+"";
+        AmountText.text = GetChurch3()+"";
     }
     public void OnButtonClickZeolite1()
     {
         if (GetGold() <= UpgradeZeolite1Cost)
             Debug.Log("골드 부족");
         else
+        {
             SubGold(UpgradeZeolite1Cost);
+            AddZeolite1(UpgradeZeolite1Cost);
+        }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(3).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(3).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeZeolite1Cost+"";
+        AmountText.text = GetZeolite1()+"";
     }
     public void OnButtonClickZeolite2()
     {
         if (GetGold() <= UpgradeZeolite2Cost)
             Debug.Log("골드 부족");
         else
+        {
             SubGold(UpgradeZeolite2Cost);
+            AddZeolite2(UpgradeZeolite2Cost);
+        }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(4).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(4).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeZeolite2Cost+"";
+        AmountText.text = GetZeolite2()+"";
     }
     public void OnButtonClickZeolite3()
     {
         if (GetGold() <= UpgradeZeolite3Cost)
             Debug.Log("골드 부족");
         else
+        {
             SubGold(UpgradeZeolite3Cost);
+            AddZeolite2(UpgradeZeolite3Cost);
+        }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(5).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(5).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeZeolite3Cost+"";
+        AmountText.text = GetZeolite3()+"";
     }
     public void OnButtonClickCity1()
     {
         if (GetGold() <= UpgradeCity1Cost)
             Debug.Log("골드 부족");
         else
+        {
             SubGold(UpgradeCity1Cost);
+            AddCity1(UpgradeCity1Cost);
+        }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(6).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(6).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeCity1Cost+"";
+        AmountText.text = GetCity1()+"";
     }
     public void OnButtonClickCity2()
     {
         if (GetGold() <= UpgradeCity2Cost)
             Debug.Log("골드 부족");
         else
+        {
             SubGold(UpgradeCity2Cost);
+            AddCity1(UpgradeCity2Cost);
+        }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(7).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(7).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeCity2Cost+"";
+        AmountText.text = GetCity2()+"";
     }
     public void OnButtonClickCity3()
     {
         if (GetGold() <= UpgradeCity3Cost)
             Debug.Log("골드 부족");
         else
+        {
             SubGold(UpgradeCity3Cost);
+            AddCity3(UpgradeCity3Cost);
+        }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(8).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = BuildingPanel.transform.GetChild(0).GetChild(0).GetChild(8).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeCity3Cost+"";
+        AmountText.text = GetCity3()+"";
     }
     public void OnButtonClickWeapon1()
     {
         if (GetGold() <= UpgradeWeapon1Cost)
             Debug.Log("골드 부족");
         else
+        {
             SubGold(UpgradeWeapon1Cost);
+            AddWeapon1(UpgradeWeapon1Cost);
+        }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = WeaponPanel.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = WeaponPanel.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeWeapon1Cost+"";
+        AmountText.text = GetWeapon1()+"";
     }
     public void OnButtonClickWeapon2()
     {
         if (GetGold() <= UpgradeWeapon2Cost)
             Debug.Log("골드 부족");
         else
+        {
             SubGold(UpgradeWeapon2Cost);
+            AddWeapon2(UpgradeWeapon2Cost);
+        }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = WeaponPanel.transform.GetChild(0).GetChild(0).GetChild(1).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = WeaponPanel.transform.GetChild(0).GetChild(0).GetChild(1).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeWeapon2Cost+"";
+        AmountText.text = GetWeapon2()+"";
     }
     public void OnButtonClickWeapon3()
     {
         if (GetGold() <= UpgradeWeapon3Cost)
             Debug.Log("골드 부족");
         else
+        {
             SubGold(UpgradeWeapon3Cost);
+            AddWeapon3(UpgradeWeapon3Cost);
+        }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = WeaponPanel.transform.GetChild(0).GetChild(0).GetChild(2).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = WeaponPanel.transform.GetChild(0).GetChild(0).GetChild(2).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeWeapon3Cost+"";
+        AmountText.text = GetWeapon3()+"";
     }
     public void OnButtonClickWeapon4()
     {
         if (GetGold() <= UpgradeWeapon4Cost)
             Debug.Log("골드 부족");
         else
+        {
             SubGold(UpgradeWeapon4Cost);
+            AddWeapon4(UpgradeWeapon4Cost);
+        }
+        //ResourcePanel 의 ViewPort -> Content -> Panel -> Button
+
+        CostText = WeaponPanel.transform.GetChild(0).GetChild(0).GetChild(3).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        AmountText = WeaponPanel.transform.GetChild(0).GetChild(0).GetChild(3).GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        
+        //이제 텍스트 변경
+        CostText.text = UpgradeWeapon4Cost+"";
+        AmountText.text = GetWeapon4()+"";
     }
 }
